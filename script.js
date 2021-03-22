1ec78d3191d1ca85163ca8887143ec2ceb4a8138
santosh kumar pandit <skpandit659@gmail.com>
2021-03-22 21:41:43 +0545

    class add

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
-    const header=document.querySelector('.header');
+    const header=document.querySelector('.Html');
+    const header=document.querySelector('.Css');
+    const header=document.querySelector('.Bootstrap');
+    const header=document.querySelector('.Php');
+    const header=document.querySelector('.Python');
+    const header=document.querySelector('.JavaScript');
+    const header=document.querySelector('.ReactNative');
+    const header=document.querySelector('.MySql');
+    const header=document.querySelector('.Sqlite3');
+    const header=document.querySelector('.C++">');
+    const header=document.querySelector('.C');
     const mouseOver=()=>{
         console.log('enter');
         anime({
@@ -18,8 +28,6 @@
                 value: 250,
                 duration: 800
               },
-              
-              
             duration: 4000,
             easing: 'easeOutElastic(1, .8)',
             loop: true
