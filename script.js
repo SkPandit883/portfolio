7ba346dc9f56ed5be59a98355fceddda7bffa9c8
santosh kumar pandit <skpandit659@gmail.com>
2021-12-01 21:36:55 +0545

    fix

--- script.js
+++ script.js
@@ -1,182 +1,185 @@
 anime({
-  targets: '.Html_lang',
+    targets: '.Html_lang',
 
-  translateX:[{
-    value:200,
-    duration:500},{
-      value:-200,
-      duration:2000
-    }
-  ],
-  rotate:{
-    value: '+=2turn', // 0 + 2 = '2turn'
-    duration: 1800,
+    translateX: [{
+        value: 200,
+        duration: 500
+    }, {
+        value: -200,
+        duration: 2000
+    }],
+    rotate: {
+        value: '+=2turn', // 0 + 2 = '2turn'
+        duration: 1800,
+        easing: 'easeInOutSine'
+    },
+    translateY: [{
+        value: 200,
+        duration: 2000
+    }],
+
+    scale: {
+        value: 2,
+        duration: 1600,
+        delay: 800,
+        easing: 'easeInOutQuart'
+    },
+    direction: 'alternate',
+    loop: true,
     easing: 'easeInOutSine'
-  },
-  translateY:[{
-      value:200,
-      duration:2000
-    }
-  ],
-
-  scale: {
-    value: 2,
-    duration: 1600,
-    delay: 800,
-    easing: 'easeInOutQuart'
-  },
-  direction: 'alternate',
-  loop: true,
-  easing: 'easeInOutSine'
 });
 
 anime({
-  targets:'.css',
-  translateX:[{
-    value:-200,
-    duration:2000,
-  },{
-    value:200,
-    duration:3000,
-  }],
+    targets: '.css',
+    translateX: [{
+        value: -200,
+        duration: 2000,
+    }, {
+        value: 200,
+        duration: 3000,
+    }],
 
-  translateY:[{
-    value:400,
-    duration:3000
-  }],
-  scale:{
-    value:-2,
-    duration: 1600,
-    delay: 800,
-    easing: 'easeInOutQuart'
-  },
-  rotate:{
-    value:'+=2turn',
-    duration:5000
-    
-  },
+    translateY: [{
+        value: 400,
+        duration: 3000
+    }],
+    scale: {
+        value: -2,
+        duration: 1600,
+        delay: 800,
+        easing: 'easeInOutQuart'
+    },
+    rotate: {
+        value: '+=2turn',
+        duration: 5000
 
-  loop:true
+    },
+
+    loop: true
 });
 anime({
-  
-  targets:'.react',
-  scale:{
-    value:-1,
-    duration: 1600,
-    delay: 800,
-    easing: 'easeInOutQuart'
-  },
-  rotate:{
-    value:'+=2turn',
-    duration:5000
-  },
-  loop:true
+
+    targets: '.react',
+    scale: {
+        value: -1,
+        duration: 1600,
+        delay: 800,
+        easing: 'easeInOutQuart'
+    },
+    rotate: {
+        value: '+=2turn',
+        duration: 5000
+    },
+    loop: true
 
 })
 anime({
-  targets:'.tailwind',
-  translateY:{
-    value:-250,
-    duration:10000
-  },
-  translateX:{
-    value:500,
-    duration:10000
-  },
-  translateY:{
-    value:-200,
-    duration:5000
-  },
-  translateY:{
-    value:-200,
-    duration:5000
-  },
-  rotate:{value:300, duration:3000},
-  translateY:{
-    value:-200,
-    duration:5000
-  },
-  rotate:{value:300, duration:3000},
-  
-  duration:300,
-  loop:true,
+    targets: '.tailwind',
+    translateY: {
+        value: -250,
+        duration: 10000
+    },
+    translateX: {
+        value: 500,
+        duration: 10000
+    },
+    translateY: {
+        value: -200,
+        duration: 5000
+    },
+    translateY: {
+        value: -200,
+        duration: 5000
+    },
+    rotate: { value: 300, duration: 3000 },
+    translateY: {
+        value: -200,
+        duration: 5000
+    },
+    rotate: { value: 300, duration: 3000 },
+
+    duration: 300,
+    loop: true,
 })
 anime({
-  targets:'.bootstrap',
-  translateY:{value:50, duration:10000},
-  translateX:{value:50, duration:100000},
-  rotate:{value:360,duration:3000},
-  scale:{value:-1,duration:5000},
-  scale:{value:-0.4,duration:5000},
-  translateX:{value:200, duration:5000},
-  translateY:{value:50, duration:5000},
-  rotate:{value:360,duration:10000},
-  rotate:{value:300, duration:3000},
+    targets: '.bootstrap',
+    translateY: { value: 50, duration: 10000 },
+    translateX: { value: 50, duration: 100000 },
+    rotate: { value: 360, duration: 3000 },
+    scale: { value: -1, duration: 5000 },
+    scale: { value: -0.4, duration: 5000 },
+    translateX: { value: 200, duration: 5000 },
+    translateY: { value: 50, duration: 5000 },
+    rotate: { value: 360, duration: 10000 },
+    rotate: { value: 300, duration: 3000 },
 
 
-  loop:true,
+    loop: true,
 })
 anime({
-  targets:'.python',
-  translateX:{value:200, duration:100000},
-  rotate:{value:360,delay:2000,duration:3000},
-  translateY:{value:200, duration:100000},
-  scale:{value:-1,delay:3000,duration:3000},
-  translateX:{value:200, duration:100000},
-  rotate:{value:300,delay:2000,duration:3000},
-  loop:true,
+    targets: '.python',
+    translateX: { value: 200, duration: 100000 },
+    rotate: { value: 360, delay: 2000, duration: 3000 },
+    translateY: { value: 200, duration: 100000 },
+    scale: { value: -1, delay: 3000, duration: 3000 },
+    translateX: { value: 200, duration: 100000 },
+    rotate: { value: 300, delay: 2000, duration: 3000 },
+    loop: true,
 
 })
 anime({
-  targets:'.node',
-  translateX:[{
-    value:200,
-    duration:2000,
-  },{
-    value:-200,
-    duration:3000,
-  }],
-  scale:{
-    value:-2,
-    duration: 1600,
-    delay: 800,
-    easing: 'easeInOutQuart'
-  },
-  rotate:{
-    value:'+=2turn',
-    duration:5000
-    
-  },
+    targets: '.node',
+    translateX: [{
+        value: 200,
+        duration: 2000,
+    }, {
+        value: -200,
+        duration: 3000,
+    }],
+    scale: {
+        value: -2,
+        duration: 1600,
+        delay: 800,
+        easing: 'easeInOutQuart'
+    },
+    rotate: {
+        value: '+=2turn',
+        duration: 5000
 
-  loop:true,
+    },
+
+    loop: true,
 })
 anime({
-  targets:'.first-container',
-  scale:{value:-0.4,duration:3000,delay:2000},
-  rotate:{
-    value:360,
-    duration:5000
-    
-  },
-  translateY:{value:-200,duration:5000},
-  loop:true
+    targets: '.first-container',
+    scale: { value: -0.4, duration: 3000, delay: 2000 },
+    rotate: {
+        value: 360,
+        duration: 5000
+
+    },
+    translateY: { value: -200, duration: 5000 },
+    loop: true
+
 })
 anime({
-  targets:'.second-container',
-  scale:{value:-0.4,duration:1000,delay:2000},
-  rotate:{
-    value:30,
-    duration:5000
-    
-  },
-  loop:true
+    targets: '.second-container',
+    scale: { value: -0.4, duration: 1000, delay: 2000 },
+    rotate: {
+        value: 30,
+        duration: 5000
+
+    },
+    loop: true
 })
 anime({
-  targets:'.php',
-  rotate:{value:'+=1turn',duration:2000},
-  translateY:{value:-200,duration:1000},
-  translateX:{value:-200,duration:2000},
-  loop:true,
+    targets: '.php',
+    rotate: { value: '+=1turn', duration: 2000 },
+    translateY: { value: -200, duration: 1000 },
+    translateX: { value: -200, duration: 2000 },
+    loop: true,
 })
-console.log(Math.floor(Math.random()*100)-20);
+anime({
+    target: ''
+})
+console.log(Math.floor(Math.random() * 100) - 20);
